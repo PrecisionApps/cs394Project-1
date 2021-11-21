efb13ca890fd8fd04084c51c15c0e0c9e6d8b955
PrecisionApps <73882757+PrecisionApps@users.noreply.github.com>
2021-11-22 00:11:04 +0300

    Add files via upload

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener({ view -> onClick(view) })
 
@@ -21,7 +31,16 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         //fillSampleTodos()
-        recyclerView.adapter = TodoListAdapter()
+        //recyclerView.adapter = TodoListAdapter()
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navigationHost) as NavHostFragment
+
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupWithNavController(binding.navigationHost, navHostFragment.navController)
+        //to displau the hamburger menu icon for nav drawer
+        NavigationUI.setupActionBarWithNavController(this,navHostFragment.navController, drawerLayout)
     }
 
     val todos = mutableListOf<TodoData>()
